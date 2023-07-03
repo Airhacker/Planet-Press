383b3060cac3a372de1d76eabde0aa8e4e2b2ef0
Umair Ahmed <umairahmed.dev@gmail.com>
2023-07-03 00:23:41 -0500

    react query implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
+"use client";
+
+import { useQuery } from "react-query";
+
 export default function Home() {
-  return <main>hello</main>;
+  //Fethcher function
+  const getFacts = async () => {
+    const res = await fetch(
+      "https://facts-by-api-ninjas.p.rapidapi.com/v1/facts"
+    );
+    return res.json();
+  };
+
+  const { data, status, error, isLoading } = useQuery("randomFacts", getFacts);
+
+  // Error and loading states
+  if (error) return <div>Something went wrong ...</div>;
+  if (isLoading) return <div>Loading ...</div>;
+
+  return (
+    <main>
+      <div>
+        <h1>Random Facts</h1>
+        <p>{data.randomFacts}</p>
+      </div>
+    </main>
+  );
 }
